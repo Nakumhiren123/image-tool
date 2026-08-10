3f23f6cd525235ef4e2d625499d639804cf17edf
Hiren Nakum <nakumhiren2203@gmail.com>
2026-08-10 13:59:17 +0530

    Fix backend for Vercel

--- backend/app.js
+++ backend/app.js
@@ -69,6 +69,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`⚡ PicCraft Express + Sharp + PostgreSQL Backend running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`⚡ PicCraft Express + Sharp + PostgreSQL Backend running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
